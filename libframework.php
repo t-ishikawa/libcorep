9e4d9a19e948bdbd2b6eaa5c19587be17225bb68
ISHIKAWA Takahiro <t.ishikawa@itlabj.com>
2015-05-08 22:05:05 +0900

    * Common functions added.

--- libframework.php
+++ libframework.php
@@ -5,12 +5,12 @@
  *
  * @author   ISHIKAWA Takahiro <t.ishikawa@itlabj.com>
  * @see      README.txt, LICENCE.txt (LGPL 2.1)
- * @version  7.2  (PHP 5.3 or above required)
+ * @version  7.4  (PHP 5.3 or above required)
  */
 
 define('FRAMEWORK_NAME'		, 'libframework');
-define('FRAMEWORK_VERSION'	, '7.2');
-define('FRAMEWORK_UPDATE'	, '2015-02-18');
+define('FRAMEWORK_VERSION'	, '7.3');
+define('FRAMEWORK_UPDATE'	, '2015-05-08');
 
 /**
  * Framework class.
@@ -65,6 +65,7 @@ class Framework {
 		syslog($lv, $v."\n"); 
 	}
 }
+
 class FrameworkException extends Exception {
 	function __construct($msg=null, $code=0, Exception $prev=null) {
 		if (is_array($msg)) 
@@ -73,18 +74,30 @@ class FrameworkException extends Exception {
 	}
 }
 
+
 function is_posint($v) {
 	return (is_numeric($v) && floor($v)==$v && $v>0) ? true : false;
 }
+
 function is_posnum($v) {
 	return (is_numeric($v) && $v>0) ? true : false;
 }
+
+function array_sum_safe($a, $keys=false) {
+	$r = 0;
+	if (!is_array($keys)) $keys = array_keys($a);
+	foreach ($keys as $k) if (($v=str_replace(',','',trim($a[$k]))) && is_numeric($v))
+		$r += $v;
+	return $r;
+}
+
 function array_overwrite($a, $b) {
 	if (!is_array($b)) return false;
 	foreach ($b as $k=>$v) 
 		$a[$k] = $v;
 	return $a;
 }
+
 function array_overwrite_recursive($a, $b) {
 	if (!is_array($b)) return false;
 	foreach ($b as $k=>$v) {
@@ -95,18 +108,39 @@ function array_overwrite_recursive($a, $b) {
 	}
 	return $a;
 }
+
 function mb_trim($v) {
 	return preg_replace("/^[\s\r\n\t　](.*)[\s\r\n\t　]$/msu", "\${1}", $v);
 }
+
 function mb_basename($v) {
 	return preg_replace("/^.*\/(.*?)$/u", "\${1}", $v);
 }
+
 function filename($v) {
 	return preg_replace('/^(.*)\.(.*?)$/', "\${1}", mb_basename($v));
 }
+
+function fileext($v) {
+	return preg_replace('/^(.*)\.(.*?)$/', "\${2}", mb_basename($v));
+}
+
 function formatdate($fmt, $ts) {
 	return $ts ? date($fmt, strtotime($ts)) : null;
 }
+
+function formatnum($v, $format='%!.0n') {
+	return (is_numeric($v) ? money_format($format, $v) : false);
+}
+
+function formatunit($v, $unit_a, $div=1000, $format='%!.0n') {
+	$r = $v;
+	foreach ($unit_a as $i=>$unit)
+		if ($v >= ($vv = pow($div,$i)))
+			$r = formatnum($v / $vv, ($i==0?'%!n':$format)).$unit;
+	return $r;
+}
+
 function splitword($s) {
 	if ($a = preg_split('/[　 \r\n\t]/msu',$s))
 		foreach ($a as $k=>$v)
@@ -114,5 +148,19 @@ function splitword($s) {
 				$r[] = $b;
 	return empty($r) ? false : $r;
 }
-// vim: ts=4 
+
+function decode_array($a) {
+	if ($b = explode(',', $a)) foreach ($b as $v)
+		$r[] = str_replace('&comma;',',',trim($v));
+	return !empty($r) ? $r : false;
+}
+
+function decode_hash($kv) {
+	if ($b = explode(',', $kv)) foreach ($b as $v)
+		if (preg_match('/^(.*?)=(.*)$/', $v, $m))
+			$r[$m[1]] = str_replace('&equal;','=',$m[2]);
+	return !empty($r) ? $r : false;
+}
+
+// vim: set ts=4:
 ?>
